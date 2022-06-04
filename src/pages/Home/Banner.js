9ab2014c30ea219ed4be0783c9b34db63e04999f
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-04 10:04:49 +0600

    banner change

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img className='sm:max-w-xs xl:max-w-xl' src={bannerImage} alt='banner' />
                 <div className='px-5'>
-                    <h1 className="md:text-4xl xl:text-5xl font-semibold text-primary">Manufacturing Bay</h1>
+                    <h1 className="md:text-4xl xl:text-5xl font-semibold text-primary">Manufacturing Bay Banner will update</h1>
                     <p className="py-6 text-justify">Welcome to manufacturing bay , Here we manufacture car parts . we manufacture car brake disk , knob , piston , turbine, rime , speedometer ect</p>
 
                     <button className="btn btn-primary text-white">
