f3c977b516020ff5b8b0b12881d0e51e8fa658f5
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-06-04 11:27:15 +0600

    banner update

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { BsChevronDoubleDown } from 'react-icons/bs';
-import bannerImage from '../../assest/image/bannerImage.jpg'
+import hand1 from '../../assest/image/hand1.jpg'
+import hand2 from '../../assest/image/hand2.jpg'
 
 const Banner = () => {
     return (
 
-        <div className="hero ">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img className='sm:max-w-xs xl:max-w-xl' src={bannerImage} alt='banner' />
-                <div className='px-5'>
-                    <h1 className="md:text-4xl xl:text-5xl font-semibold text-primary">Manufacturing Bay Banner will update</h1>
-                    <p className="py-6 text-justify">Welcome to manufacturing bay , Here we manufacture car parts . we manufacture car brake disk , knob , piston , turbine, rime , speedometer ect</p>
+        <>
+            <div className="hero min-h-screen" style={{ backgroundImage: `url(${hand1})` }}>
+                <div className="hero-overlay bg-opacity-0 "></div>
 
-                    <button className="btn btn-primary text-white">
-                        Explore More
-                        <span className='p-2 text-xl'><BsChevronDoubleDown /></span>
-                    </button>
+
+                <div className="hero-content text-center text-neutral-content mr-auto">
+                    <div className="max-w-xl">
+                        <h1 className="md:text-4xl xl:text-5xl font-semibold text-primary">Manufacturing Bay Banner will update</h1>
+                        <p className="py-6 text-justify text-zinc-800">Welcome to manufacturing bay , Here we manufacture car parts . we manufacture car brake disk , knob , piston , turbine, rime , speedometer ect</p>
+
+                        <button className="btn btn-primary text-white">
+                            Explore More
+                            <span className='p-2 text-xl'><BsChevronDoubleDown /></span>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
+
     );
 };
 
